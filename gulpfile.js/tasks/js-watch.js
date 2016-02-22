b288fdb1255f5c6f7a7242081f8795a5d4904285
Geert Fokke <geert@tamtam.nl>
2016-02-22 13:23:08 +0100

    fixed js watch fixed vertical center mixin margin

--- gulpfile.js/tasks/js-watch.js
+++ gulpfile.js/tasks/js-watch.js
@@ -7,80 +7,38 @@ var js							= require('./js');
 
 var config                      = require('../config');
 var log                         = require('../src/debug/log');
-var objectDiff					= require('../src/object/diff');
 var requireCached     			= require('../src/gulp/require-cached');
 
 var browserSync                 = requireCached('browser-sync');
 var gulp                        = requireCached('gulp');
 var webpack                     = requireCached('webpack');
+
 // @formatter:on
 
 
 gulp.task( 'js-watch', function jsWatch ( callback ) {
 
 	var initialCompile = true;
-	var fileTimestamps;
+	var previousStats;
 
-	js.apply( this, [ initialCompile ? callback : undefined ] ).watch( 200, function ( error, stats ) {
+	webpack( js.createOptions().webpack ).watch( 200, function ( error, stats ) {
 
-		if( error ) log.error( {
-			sender: 'js-watch',
-			data: [ error ]
-		} );
+		js.onWebpackCallback( error, stats, previousStats );
 
-		//log.info( {
-		//	sender: 'js-watch',
-		//	data: [ status ]
-		//} );
+		if( !stats.compilation.errors && stats.compilation.errors.length ) browserSync.reload();
+		
+		if( initialCompile ) {
 
-		var currentTimestamps = stats.compilation.fileTimestamps;
-
-		if( fileTimestamps && !_.isEmpty( fileTimestamps ) ) {
-
-			var diff = objectDiff( fileTimestamps, currentTimestamps );
-			var sourceRoot = path.resolve( config.source.getPath( 'root' ), '../' );
-			var changedFiles = [];
-
-			for ( var filePath in diff ) changedFiles.push( filePath.replace( sourceRoot, '' ) );
-
-			log.info( {
-				sender: 'js-watch',
-				message: 'changed files:',
-				data: changedFiles.join( '\n\t' )
-			} )
+			initialCompile = false;
+			callback();
 
 		}
 
-
-		var compErrors = stats.compilation.errors;
-		if( compErrors && compErrors.length ) {
-
-			for ( var i = 0, leni = compErrors.length; i < leni; i++ ) {
-				var compError = compErrors[ i ];
-				log.error( {
-					sender: 'js-watch',
-					name: compError.name,
-					message: compError.message
-				} )
-			}
-
-		} else {
-
-			browserSync.reload();
-
-		}
-
-
-		if( !_.isEmpty( currentTimestamps ) ) fileTimestamps = currentTimestamps;
-
+		previousStats = stats;
+		
 	} );
 
-	initialCompile = false;
-
-
 } );
-
-module.exports = js;
 
 
 
