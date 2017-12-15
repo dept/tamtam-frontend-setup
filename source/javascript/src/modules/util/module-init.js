7f583f5a7cab2a4c468ec12e7153e961f6aa577e
Adrian Klingen <adrian@tamtam.nl>
2017-12-15 10:33:13 +0100

    Module init error fixes

--- source/javascript/src/modules/util/module-init.js
+++ source/javascript/src/modules/util/module-init.js
@@ -55,11 +55,12 @@ function ModuleInit(selector, constructor, opt_arguments) {
     if (Function.prototype.name === undefined && Object.defineProperty !== undefined) {
         Object.defineProperty(Function.prototype, 'name', {
             get: function () {
-                var funcNameRegex = /function\s([^(]{1,})\(/;
-                var results = (funcNameRegex).exec((this).toString());
+                const funcNameRegex = /function\s([^(]{1,})\(/;
+                const results = (funcNameRegex).exec((this).toString());
                 return (results && results.length > 1) ? results[1].trim() : "";
             },
             set: function () {
+                // Empty function to prevent set is not a function
             }
         });
     }
