e730343d374c4267538233927114d367f35fc0f1
Jeroen Reumkens <jeroen.reumkens@tamtam.nl>
2018-01-26 13:19:15 +0100

    Added javascript config file for singletons, to cleanup main.js more:

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -6,21 +6,13 @@
 import 'babel-polyfill';
 
 // Import utilities.
-import env from './src/system/environment';
 import moduleInit from './src/modules/util/module-init';
-import { initServiceWorker, removeServiceWorker } from './src/modules/util/sw';
 
 // Import Singletons
 import './src/modules/util/detect-touch';
 
 // Import modules.
 import ExampleModule from './src/modules/example';
-
-if (!env.isLocal()) {
-    initServiceWorker();
-} else {
-    removeServiceWorker();
-}
 
 // Initialize modules.
 moduleInit( '[js-hook-module-example]', ExampleModule );
