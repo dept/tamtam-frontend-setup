9fde4286a39cf12ac0822fa25b0f20035e3ddd33
Adrian Klingen <adrian@tamtam.nl>
2018-05-19 14:52:27 +0200

    removed stage 2

--- gulpfile.js/tasks/js.js
+++ gulpfile.js/tasks/js.js
@@ -71,8 +71,7 @@ const configureBabelLoader = (browserlist) => {
                         targets: {
                             browsers: browserlist,
                         },
-                    }],
-                    'stage-2'
+                    }]
                 ],
             },
         },
