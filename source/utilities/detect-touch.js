a1a55976743ec15d278a42b87714b7ca6e027b7f
Robin Treur <robin@Robins-MacBook-Pro.local>
2018-09-28 16:07:19 +0200

    Remove is-touch class if device has touch and use a mouse

--- source/utilities/detect-touch.js
+++ source/utilities/detect-touch.js
@@ -1,3 +1,5 @@
+const IS_TOUCH = 'is-touch';
+
 class DetectTouch {
 
     get isTouchDevice() {
@@ -5,12 +7,28 @@ class DetectTouch {
     }
 
     constructor() {
+        this.hasMouse = false;
+        this.mouseEvent = () => this._handleMouseEvent();
         this.touch = 'ontouchstart' in document.documentElement
             || navigator.maxTouchPoints > 0
             || navigator.msMaxTouchPoints > 0;
 
+
+        document.documentElement.addEventListener('touchstart', () => {
+            document.documentElement.removeEventListener('mousemove', this.mouseEvent);
+        });
+
+        document.documentElement.addEventListener('mousemove', this.mouseEvent);
+
         if (this.touch) {
-            document.querySelector('html').classList.add( 'is-touch' );
+            document.querySelector('html').classList.add(IS_TOUCH);
+        }
+    }
+
+    _handleMouseEvent() {
+        if(!this.hasMouse){
+            document.querySelector('html').classList.remove(IS_TOUCH);
+            this.hasMouse = true;
         }
     }
 }
