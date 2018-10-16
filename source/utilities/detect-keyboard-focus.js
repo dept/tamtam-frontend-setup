68a11cbfddb7e489ade886a4bdeeb631d530f1fd
Robin Treur <robin@Robins-MacBook-Pro.local>
2018-10-16 16:07:24 +0200

    FIxed leave with "alt tab" bug

--- source/utilities/detect-keyboard-focus.js
+++ source/utilities/detect-keyboard-focus.js
@@ -7,6 +7,7 @@ class DetectKeyboardFocus {
 
         document.addEventListener('keydown', () => this._handleKey(true), true);
         document.addEventListener('keyup', () => this._handleKey(false), true);
+        document.addEventListener('mouseleave', () => this._handleKey(false));
         document.addEventListener('focus', () => this._handleFocus(), true);
         document.addEventListener('blur', () => this._handleBlur(), true);
     }
