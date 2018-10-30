2778dc9d5e4946205aba1bbd62b70e2dc0441b5e
Adrian Klingen <adrian@tamtam.nl>
2018-10-30 11:46:43 +0100

    Changed behavior of default build config overrides

--- build-config/environment-overrides/dist.js
+++ build-config/environment-overrides/dist.js
@@ -1,10 +1,29 @@
-const distOverrideConfig = {
+const path = require('path');
+const projectDirectory = process.env.OLDPWD || path.normalize(process.env.INIT_CWD);
+const buildFolder = 'build';
+
+const deployOverrideConfig = {
     minify: false,
+    source: {
+        sw: {
+            path: path.resolve(projectDirectory, buildFolder, 'assets'),
+            strip: path.resolve(projectDirectory, buildFolder)
+        }
+    },
     dest: {
         root: {
-            path: './build'
+            path: './' + buildFolder
+        },
+        html: {
+            path: '<%= root %>/html'
+        },
+        assets: {
+            path: '<%= root %>/assets'
+        },
+        sw: {
+            path: '<%= root %>'
         }
     }
 }
 
-module.exports = distOverrideConfig;
+module.exports = deployOverrideConfig;
