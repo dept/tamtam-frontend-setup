895e2c4e91a18cd0391f33ba9a0be1c55e1cf7d4
Adrian Klingen <adrian@tamtam.nl>
2019-10-25 12:45:24 +0200

    Updated main.js

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -4,17 +4,12 @@
  * JS Main entry file
  \*------------------------------------*/
 import './config'
-// import Example from '@components/example';
-// Initialize modules.
-// Sync
 import '@utilities/detect-keyboard-focus'
-// Import utilities.
-// Import modules.
 import '@utilities/detect-touch'
 
-// Import Singletons
 import moduleInit from '@utilities/module-init'
-// moduleInit.sync('[js-hook-module-example]', Example);
 
-// Async
-moduleInit.async('[js-hook-module-example]', () => import('@components/example'))
+// import Example from '@components/example' // Sync
+// moduleInit.sync('[js-hook-module-example]', Example) // Sync
+
+moduleInit.async('[js-hook-module-example]', () => import('@components/example')) // Async
