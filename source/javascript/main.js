3dc8ac34df0ca6c513f5cceb62b9953e1cfb55df
Adrian Klingen <adrian@tamtam.nl>
2020-12-14 11:26:54 +0100

    Fixes

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -3,10 +3,11 @@
 /*------------------------------------*\
  * JS Main entry file
  \*------------------------------------*/
-import moduleInit from '@utilities/module-init'
-import '@utilities/detect-touch'
 import './config'
+import '@utilities/detect-touch'
 import '@utilities/detect-keyboard-focus'
+
+import moduleInit from '@utilities/module-init'
 
 // import Example from '@components/example' // Sync
 // moduleInit.sync('[js-hook-module-example]', Example) // Sync
