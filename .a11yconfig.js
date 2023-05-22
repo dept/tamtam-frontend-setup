832312a1244f064b4180bfd96fce4a623f174fbb
Adrian Klingen <adrian.klingen@deptagency.com>
2023-05-22 16:06:33 +0200

    Update ignore comment

--- .a11yconfig.js
+++ .a11yconfig.js
@@ -12,8 +12,8 @@ module.exports = {
     standard: 'WCAG2AA',
     includeNotices: true,
     includeWarnings: true,
-    // For possible options see: https://squizlabs.github.io/HTML_CodeSniffer/Standards/WCAG2/
-    // Example: "Principle4.Guideline4_1.4_1_1.F77" for ignoring duplicate IDs
+    // For possible options see: https://dequeuniversity.com/rules/axe/html/4.7 and the other versions
+    // Example: "duplicate-id" for ignoring duplicate IDs
     ignore: [],
     viewport: {
       width: 1280,
